d288f3d555aa7657d2c2e314d7472de86ba9c0cf
Alex Lee <hyunho98@gmail.com>
2023-11-14 12:11:22 -0500

    commit tree 5a454314e898df9cec32b87e6cb4ad0fab096c0d

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,21 @@
 require_relative './stack'
 
 # your code here
+def balancing_parentheses(string)
+    stack = Stack.new
+    c = ""
+
+    string.each_char do |char|
+        if stack.size == 0
+            stack.push(char)
+        elsif char == "("
+            stack.push(char)
+        elsif stack.peek == "(" && char == ")"
+            stack.pop
+        else
+            stack.push(char)
+        end
+    end
+
+    stack.size
+end
